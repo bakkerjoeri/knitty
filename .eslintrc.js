91f89b9429348eb2f8940f079322525bb2de8428
Joeri Bakker <bakkerjoeri@gmail.com>
2017-08-15 10:41:14 +0200

    Added editor and eslinter

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	"extends": "eslint:recommended",
+	"parser": "babel-eslint",
+	"env": {
+		"browser": true,
+		"es6": true,
+		"amd": true,
+	},
+	"rules": {
+		"quotes": ["warn", "single"],
+		"no-var": "warn",
+		"prefer-template": "warn",
+		"eqeqeq": "error"
+	}
+}
